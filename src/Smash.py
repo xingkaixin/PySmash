5648bdebd9ec62a175b0ec94d4c543da8407cec6
xingkaixin <xingkaixin@gmail.com>
2014-08-22 22:18:40 +0800

    add unittest

--- src/Smash.py
+++ src/Smash.py
@@ -41,6 +41,3 @@ class Smash:
         return result
 
 
-s = Smash()
-a = raw_input("abc:")
-print s.complareSmash(a)
---
+++ src/Test1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+__author__ = 'XingKaiXin.me'
+
+import unittest
+import Smash
+
+
+class Test1(unittest.TestCase):
+    def setUp(self):
+        self.s = Smash.Smash()
+        # pass
+
+    def tearDown(self):
+        pass
+
+    def testsmash(self):
+        print self.s.complareSmash(1234)
+
+#
+# if __name__ =='__main__':
+#     unittest.main()
