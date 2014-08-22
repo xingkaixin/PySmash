35ed0a31b74de0c819894605bea1b01a3710a3e7
xingkaixin <xingkaixin@gmail.com>
2014-08-22 22:05:34 +0800

    create Smash class

--- src/Smash.py
+++ src/Smash.py
@@ -4,41 +4,43 @@ __author__ = 'XingKaiXin.me'
 import random
 
 
-# 生成一个4位不重复数字
-def genSmash():
-    single_smash = range(1, 9)
+class Smash:
     smash = []
-    for i in range(0, 4):
-        randint = random.choice(single_smash)
-        single_smash.remove(randint)
-        smash.append(randint)
-    return smash
+
+    # 生成一个4位不重复数字
+    def __init__(self):
+        single_smash = range(1, 9)
+        for i in range(0, 4):
+            randint = random.choice(single_smash)
+            single_smash.remove(randint)
+            self.smash.append(randint)
 
 
-# 单个数字在Smash中存在与否，位置多少
-def findnum(onenum, smash):
-    if smash.count(onenum) > 0:
-        return [1, smash.index(onenum) + 1]
-    else:
-        return [0, 0]
-
-
-# 4位数字猜中了Smash多少
-def complareSmash(guessnum):
-    exist = 0
-    result = []
-    smash = genSmash()
-    print smash
-    for i in range(4):
-        a = int(str(guessnum)[i])
-        b = findnum(a, smash)
-        exist += b[0]
-        if b[1] == i + 1:
-            result.append(1)
+    # 单个数字在Smash中存在与否，位置多少
+    def findnum(self, onenum):
+        if self.smash.count(onenum) > 0:
+            return [1, self.smash.index(onenum) + 1]
         else:
-            result.append(0)
-    result.append(exist)
-    return result
+            return [0, 0]
 
 
-print complareSmash(1234)
+    # 4位数字猜中了Smash多少
+    def complareSmash(self, guessnum):
+        exist = 0
+        result = []
+        print self.smash
+        for i in range(4):
+            a = int(str(guessnum)[i])
+            b = self.findnum(a)
+            exist += b[0]
+            if b[1] == i + 1:
+                result.append(1)
+            else:
+                result.append(0)
+        result.append(exist)
+        return result
+
+
+s = Smash()
+a = raw_input("abc:")
+print s.complareSmash(a)
